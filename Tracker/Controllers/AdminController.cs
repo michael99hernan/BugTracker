6faee867f050707d8c60b1531901aed2ee5eccb4
michael99hernan <55895586+michael99hernan@users.noreply.github.com>
2020-05-14 23:56:50 -0500

    Added admin views

--- Tracker/Controllers/AdminController.cs
+++ Tracker/Controllers/AdminController.cs
@@ -1,21 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Differencing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tracker.ViewModels;
-
+using Tracker.Models;
 namespace Tracker.Controllers
 {
     public class AdminController : Controller
     {
-        private RoleManager<IdentityRole> roleManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<TrackerUser> userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager,
+                               UserManager<TrackerUser> userManager)
         {
-           this.roleManager = roleManager;
+            this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -25,16 +29,16 @@ namespace Tracker.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRoleAsync(CreateRoleViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 IdentityRole identityRole = new IdentityRole
                 {
                     Name = model.RoleName
                 };
                 IdentityResult result = await roleManager.CreateAsync(identityRole);
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    return RedirectToAction("ListRoles", "admin");
                 }
 
                 foreach (IdentityError error in result.Errors)
@@ -42,10 +46,72 @@ namespace Tracker.Controllers
                     ModelState.AddModelError("", error.Description);
 
                 }
-                
+
             }
 
             return View(model);
         }
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $" Role with Id = {id} cannot be found";
+                return View("NotFound");
+
+            }
+            else
+            {
+                var model = new EditRoleViewModel
+                {
+                    Id = role.Id,
+                    RoleName = role.Name
+                };
+
+                foreach (var user in userManager.Users)
+                {
+                    if (await userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        model.Users.Add(user.UserName);
+                    }
+                }
+                return View(model);
+            }
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $" Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+
+                }
+            }
+            return View(model);
+        }
     }
+    
 }
